f9002ff29edeed58c97cebf15d127385899879f6
Icelle <icelleku@gmail.com>
2013-12-22 16:01:50 -0500

    make category tests pass

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ActiveRecord::Base
+  validates :name, presence:true, uniqueness: true
+
+  has_many :entry_categories
+  has_many :journal_entries,
+    through: :entry_categories,
+    inverse_of: :categories
 end
