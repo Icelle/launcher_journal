0296f84f67eef8a9d966f94f23d315d945b0f2c3
Icelle <icelleku@gmail.com>
2013-12-22 16:01:44 -0500

    Add tests for category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should have_valid(:name).when('TDD')}
+  it {should_not have_valid(:name).when('', nil)}
+  it { should have_many(:entry_categories) }
+  it { should have_many(:journal_entries) }
+
+  it "doesn't allow duplicates" do
+    FactoryGirl.create(:category, {name: 'TDD'})
+    dup = FactoryGirl.build(:category, {name: 'TDD'})
+    dup.should_not be_valid
+    dup.should have(1).errors_on(:name)
+
+  end
 end
