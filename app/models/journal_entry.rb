74c590b1f6c213c1573d7175717b07076776cf06
Icelle <icelleku@gmail.com>
2013-12-22 16:02:12 -0500

    make journal entry tests pass

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -1,2 +1,9 @@
 class JournalEntry < ActiveRecord::Base
+  validates :title, presence:true
+  validates :description, presence:true
+
+  has_many :entry_categories
+  has_many :categories,
+    through: :entry_categories,
+    inverse_of: :journal_entries
 end
