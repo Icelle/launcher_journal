6b2f0e3c8a12a1904c4a6b3caf68f6fa2d9ac2b9
Icelle <icelleku@gmail.com>
2013-12-22 15:25:29 -0500

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131222200832) do
+ActiveRecord::Schema.define(version: 20131222200901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: true do |t|
-    t.string   "name"
+    t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  create_table "entry_categories", force: true do |t|
+    t.integer  "journal_entry_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "entry_categories", ["category_id"], name: "index_entry_categories_on_category_id", using: :btree
+  add_index "entry_categories", ["journal_entry_id", "category_id"], name: "index_entry_categories_on_journal_entry_id_and_category_id", unique: true, using: :btree
+  add_index "entry_categories", ["journal_entry_id"], name: "index_entry_categories_on_journal_entry_id", using: :btree
+
   create_table "journal_entries", force: true do |t|
-    t.string   "title"
-    t.text     "description"
+    t.string   "title",       null: false
+    t.text     "description", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
