0e8e64f2c2be479e7eab10d1a2e61d266377cbcb
Icelle <icelleku@gmail.com>
2013-12-23 15:21:07 -0500

    add create_and_find method for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,4 +5,20 @@ class Category < ActiveRecord::Base
   has_many :journal_entries,
     through: :entry_categories,
     inverse_of: :categories
+
+
+  # @param [String] category_str - space separated categories provided by user
+  # @return [Array] array of active record categories
+  def self.create_and_find(category_str)
+    categories = []
+    category_str.downcase.split(" ").each do |c|
+      category = Category.find_by_name(c)
+      # if not found, create it
+      if category.nil?
+        category = Category.create({name: c})
+      end
+      categories.push(category)
+    end
+    return categories
+  end
 end
